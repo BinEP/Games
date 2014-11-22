8fb0db79311216b85ddc16158bbb056b06f3253b
BinEP <bradystoffel@gmail.com>
2014-11-22 02:11:28 -0600

    Card Stuff - Go Fish

--- Games/src/goFish/Card.java
+++ Games/src/goFish/Card.java
@@ -1,8 +1,138 @@
 package goFish;
 
+import java.util.Comparator;
+
 public class Card {
 
-	public static void main(String[] args) {
+	private int suit;
+	private int card;
+	private String shown;
 
+	public Card() {
+
+		suit = (int) (Math.random() * 4) + 1;
+		card = (int) (Math.random() * 13) + 1;
+		shown = getCardFace() + getSuitIcon();
+		
+
+	}
+	
+	public String getShown() {
+		
+		return shown;
+		
+	}
+	
+	public int getSuit() {
+		return suit;
+	}
+
+	public String getSuitIcon() {
+		String suitChar = "J";
+		switch (suit) {
+		case 4:
+			suitChar = "\u2665";
+			break;
+		case 3:
+			suitChar = "\u2666";
+			break;
+		case 2:
+			suitChar = "\u2663";
+			break;
+		case 1:
+			suitChar = "\u2660";
+			break;
+		}
+		
+		return suitChar;
+
+	}
+
+	public int getCard() {
+
+		return card;
+
+	}
+
+	public String getCardFace() {
+		
+		String cardFace = "A";
+		if (card < 11 && card != 1) {
+
+			return String.valueOf(card);
+
+		} else {
+
+			switch (card) {
+
+			case 11:
+				cardFace = "J";
+				break;
+			case 12:
+				cardFace = "Q";
+				break;
+			case 13:
+				cardFace = "K";
+				break;
+			default:
+				cardFace = "A";
+				break;
+
+			}
+			return cardFace;
+
+		}
+
+	}
+	
+	public static Comparator<Card> CardSuitComparator = new Comparator<Card>() {
+
+		public int compare(Card c1, Card c2) {
+		   int cardSuit1 = c1.getSuit();
+		   int cardSuit2 = c2.getSuit();
+
+		   //ascending order
+		   return cardSuit2 - cardSuit1;
+
+		   //descending order
+		   //return StudentName2.compareTo(StudentName1);
+	    }};
+	    
+	    public static Comparator<Card> CardNumComparator = new Comparator<Card>() {
+
+	    	public int compare(Card c1, Card c2) {
+
+	    	   int cardNum1 = c1.getCard();
+	    	   int cardNum2 = c2.getCard();
+
+	    	   /*For ascending order*/
+	    	   return cardNum1-cardNum2;
+
+	    	   /*For descending order*/
+	    	   //rollno2-rollno1;
+	       }};
+	       
+	       @Override
+	       public String toString() {
+	           return "[ Card Suit = " + getSuitIcon() + ", Card Value = " + getCardFace() + "]";
+	       }
+	       
+	       @Override
+	       public boolean equals(Object o) {
+	    	   
+	    	   if (o instanceof Card) {
+	    		   
+	    		   return getCard() == ((Card) o).getCard() && getSuit() == ((Card) o).getSuit();
+	    		   
+	    	   }
+	    	   return false;
+	    	   
+	       }
+
+	public static void main(String[] args) {
+		System.out.println('\u2665');
+		System.out.println("\u2663");
+		System.out.println("\u2666");
+		System.out.println("\u2660");
 	}
 }
--- Games/src/goFish/GoFishPanel.java
+++ Games/src/goFish/GoFishPanel.java
@@ -1,18 +1,28 @@
 package goFish;
 
 import java.awt.Color;
+import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
 public class GoFishPanel extends JPanel implements ActionListener, KeyListener {
 
+	private ArrayList<Card> deck = new ArrayList<Card>();
+	private ArrayList<Card> p1Hand = new ArrayList<Card>();
+	private ArrayList<Card> p2Hand = new ArrayList<Card>();
+
 	public GoFishPanel() {
 
+		newDeck();
 		setBackground(Color.BLACK);
 
 		setFocusable(true);
@@ -20,6 +30,10 @@ public class GoFishPanel extends JPanel implements ActionListener, KeyListener {
 
 		Timer timer = new Timer(1000 / 60, this);
 		timer.start();
+
+	}
+
+	public GoFishPanel(int i) {
 
 	}
 
@@ -31,12 +45,40 @@ public class GoFishPanel extends JPanel implements ActionListener, KeyListener {
 
 	public void moves() {
 
+	}
+
+	public void newDeck() {
+		Card card = new Card();
+		for (int i = 0; i < 52; i++) {
+			while(deck.contains(card)) {
+				
+				card = new Card();
+				
+			}
+			deck.add(card);
+
+		}
+
+	}
+	
+	public ArrayList<Card> playerHand() {
+		ArrayList<Card> hand = new ArrayList<Card>();
+		for (int i = 0; i < 7; i++) {
+			hand.add(deck.get(i));
+			deck.remove(i);
+			
+		}
+		Collections.sort(hand, Card.CardSuitComparator);
+		Collections.sort(hand, Card.CardNumComparator);
+		
+		return hand;
+	}
+
+	public void shuffleDeck() {
+
 		
 		
-		
-		
-		
-		
+
 	}
 
 	@Override
@@ -54,6 +96,43 @@ public class GoFishPanel extends JPanel implements ActionListener, KeyListener {
 	@Override
 	public void keyReleased(KeyEvent e) {
 		// TODO Auto-generated method stub
+
+	}
+
+	public static void main(String[] args) {
+		
+		GoFishPanel p = new GoFishPanel(1);
+		p.runFromMain();
+
+	}
+
+	public void runFromMain() {
+
+		newDeck();
+		p1Hand = playerHand();
+		p2Hand = playerHand();
+		//Collections.sort(deck, Card.CardSuitComparator);
+		//Collections.sort(deck, Card.CardNumComparator);
+		for (Card x : deck) {
+
+			System.out.println(x.getCardFace() + x.getSuitIcon());
+			
+
+		}
+		System.out.println("Player 1 Hand");
+		for (Card x : p1Hand) {
+
+			System.out.print(x.getCardFace() + x.getSuitIcon() + "   ");
+			
+
+		}
+		System.out.println("\n Player 2 Hand");
+		for (Card x : p2Hand) {
+
+			System.out.print(x.getCardFace() + x.getSuitIcon() + "   ");
+			
+
+		}
 
 	}
 
