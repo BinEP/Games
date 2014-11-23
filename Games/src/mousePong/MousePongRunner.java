a7431b0208c23e5555b0367d27d20eda10f1c98b
BinEP <bradystoffel@gmail.com>
2014-11-22 19:41:17 -0600

    Continued Work on MousePong - Not functional

--- Games/src/mousePong/MousePongRunner.java
+++ Games/src/mousePong/MousePongRunner.java
@@ -1,8 +1,25 @@
 package mousePong;
 
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+
+import pong.PongPanel;
+
 public class MousePongRunner {
 
 	public static void main(String[] args) {
 
+		
+		JFrame frame = new JFrame("Mouse Pong");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLayout(new BorderLayout());
+		
+		MousePongPanel MousePongPanel = new MousePongPanel();
+		//wWebPong pongPanel = new WebPong();
+		frame.add(MousePongPanel, BorderLayout.CENTER);
+		
+		frame.setSize(500, 500);
+		frame.setVisible(true);
 	}
 }
