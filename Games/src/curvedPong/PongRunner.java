9e91867e8e5feeccbf61cb29b813807fe68d0a9e
BinEP <bradystoffel@gmail.com>
2014-11-23 08:07:59 -0600

    Added Curved Pong files

---
+++ Games/src/curvedPong/PongRunner.java
@@ -0,0 +1,25 @@
+package curvedPong;
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+
+
+public class PongRunner {
+
+	
+	
+	public static void main(String[] args) {
+		
+		JFrame frame = new JFrame("PONG!");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLayout(new BorderLayout());
+		
+		PongPanel pongPanel = new PongPanel();
+		//wWebPong pongPanel = new WebPong();
+		frame.add(pongPanel, BorderLayout.CENTER);
+		
+		frame.setSize(500, 500);
+		frame.setVisible(true);
+		
+	}
+}
