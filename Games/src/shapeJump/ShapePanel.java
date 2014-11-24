ed8c4257bf0f627e900df4550d3c173608e8ebc1
BinEP <bradystoffel@gmail.com>
2014-11-24 14:11:08 -0600

    Started adding drawShape method to draw the 3x3 specified shape onto the screen

--- Games/src/shapeJump/ShapePanel.java
+++ Games/src/shapeJump/ShapePanel.java
@@ -2,16 +2,28 @@ package shapeJump;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.ArrayList;
 
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
 public class ShapePanel extends JPanel implements ActionListener, KeyListener {
 
+	private int shapeWidth = 20;
+	private int squareSide = shapeWidth;
+	private int circleDiameter = shapeWidth;
+	private int borderThickness = 2;
+	private int blockSize = shapeWidth + 2 * borderThickness;
+	
+	private int ground = 350;
+	
+	private ArrayList<Integer> shapeGroupX = new ArrayList<Integer>(3);
+	
 	
 public ShapePanel() {
 
@@ -53,6 +65,23 @@ public ShapePanel() {
 		
 	}
 	
+	public void drawShape(int[] x, int[] y, Graphics2D g) {
+		
+		int pos = nextBlockX();
+		for (int i = 0; i < x.length; i++) {
+			
+			g.fillRect(pos + (x[i]-1) * blockSize + borderThickness, ground - y[i] * blockSize + borderThickness, shapeWidth, shapeWidth);
+			g.drawRect(pos + (x[i]-1) * blockSize + borderThickness/2, ground - y[i] * blockSize + borderThickness/2, shapeWidth, shapeWidth);
+			g.drawRect(pos + (x[i]-1) * blockSize, ground - y[i] * blockSize, shapeWidth, shapeWidth);
+			
+		}
+		
+		
+		
+		
+		
+	}
+	
 	public void paintComponent(Graphics g) {
 		
 		
@@ -66,6 +95,15 @@ public ShapePanel() {
 		
 		
 		
+		
+		
+	}
+	
+	public int nextBlockX() {
+		
+		shapeGroupX.add(shapeGroupX.get(2) + 170);
+		shapeGroupX.remove(0);
+		return shapeGroupX.get(shapeGroupX.size()-1);
 		
 		
 	}
