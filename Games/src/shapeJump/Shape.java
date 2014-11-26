a31193ba756e4198f35ba9ac618f5e583e4b898e
BinEP <bradystoffel@gmail.com>
2014-11-26 00:51:26 -0600

    Added Many new features!!!! - Shape class that chooses a random shape from 20 pre made ones - Scrolling shapes that are random - Start Screen - Visual block - Ground line - Timer - Jumping block - Gravity!!!! Still need to add collision detection and sizing concerns

--- Games/src/shapeJump/Shape.java
+++ Games/src/shapeJump/Shape.java
@@ -4,90 +4,292 @@ public class Shape {
 
 	public int[] x;
 	public int[] y;
-	public int numOfShapes = 1;
+	public int numOfShapes = 20;
 
 	public Shape() {
-
-		switch ((int) Math.random() * numOfShapes + 1) {
+		int shapeNum = (int) (Math.random() * (numOfShapes + 1));
+		switch (shapeNum) {
 
 		case 1:
-
+			OnexOne();
 			break;
 		case 2:
-
+			OnexTwo();
 			break;
 		case 3:
-
+			OnexThree();
 			break;
-
+		case 4:
+			TwoxOne();
+			break;
+		case 5:
+			TwoxTwo();
+			break;
+		case 6:
+			TwoxThree();
+			break;
+		case 7:
+			ThreexOne();
+			break;
+		case 8:
+			ThreexTwo();
+			break;
+		case 9:
+			ThreexThree();
+			break;
+		case 10:
+			TwoStep();
+			break;
+		case 11:
+			DownTwoStep();
+			break;
+		case 12:
+			ThreeStep();
+			break;
+		case 13:
+			DownThreeStep();
+			break;
+		case 14:
+			UpLittleT();
+			break;
+		case 15:
+			DownLittleT();
+			break;
+		case 16:
+			UpBigT();
+			break;
+		case 17:
+			DownBigT();
+			break;
+		case 18:
+			TwoxTwoPlusOne();
+			break;
+		case 19:
+			OnePlusTwoxTwo();
+			break;
+		case 20:
+			ThreexTwoPlusOne();
+			break;
+		default:
+			OnexOne();
+			break;
+		
 		}
 
 	}
 
 	public void OnexOne() {
-		
-		int[] x1 = {1};
-		int[] y1 = {1};
+
+		// ■
+		int[] x1 = { 1 };
+		int[] y1 = { 1 };
 		x = x1;
 		y = y1;
-		
+
 	}
 
 	public void OnexTwo() {
 
-		int[] x1 = {1, 1};
-		int[] y1 = {1, 2};
+		// ■
+		// ■
+		int[] x1 = { 1, 1 };
+		int[] y1 = { 1, 2 };
 		x = x1;
 		y = y1;
 	}
 
 	public void OnexThree() {
-		int[] x1 = {1, 1, 1};
-		int[] y1 = {1, 2, 3};
+		// ■
+		// ■
+		// ■
+		int[] x1 = { 1, 1, 1 };
+		int[] y1 = { 1, 2, 3 };
 		x = x1;
 		y = y1;
 	}
 
 	public void TwoxOne() {
-		int[] x1 = {1, 2};
-		int[] y1 = {1, 1};
+		// ■ ■
+		int[] x1 = { 1, 2 };
+		int[] y1 = { 1, 1 };
 		x = x1;
 		y = y1;
 	}
 
 	public void TwoxTwo() {
-		int[] x1 = {1, 2, 1, 2};
-		int[] y1 = {1, 1, 2, 2};
+		// ■ ■
+		// ■ ■
+		int[] x1 = { 1, 2, 1, 2 };
+		int[] y1 = { 1, 1, 2, 2 };
 		x = x1;
 		y = y1;
 	}
 
 	public void TwoxThree() {
-		int[] x1 = {1, 1, 1, 2, 2, 2};
-		int[] y1 = {1, 2, 3, 1, 2, 3};
+		// ■ ■
+		// ■ ■
+		// ■ ■
+		int[] x1 = { 1, 1, 1, 2, 2, 2 };
+		int[] y1 = { 1, 2, 3, 1, 2, 3 };
 		x = x1;
 		y = y1;
 	}
 
 	public void ThreexOne() {
-		int[] x1 = {1, 2, 3};
-		int[] y1 = {1, 1, 1};
+		// ■ ■ ■
+		int[] x1 = { 1, 2, 3 };
+		int[] y1 = { 1, 1, 1 };
 		x = x1;
 		y = y1;
 	}
 
 	public void ThreexTwo() {
-		int[] x1 = {1, 2, 3, 1, 2, 3};
-		int[] y1 = {1, 1, 1, 2, 2, 2};
+		// ■ ■ ■
+		// ■ ■ ■
+		int[] x1 = { 1, 2, 3, 1, 2, 3 };
+		int[] y1 = { 1, 1, 1, 2, 2, 2 };
 		x = x1;
 		y = y1;
 	}
 
 	public void ThreexThree() {
-		int[] x1 = {1, 2, 3, 1, 2, 3, 1, 2, 3};
-		int[] y1 = {1, 1, 1, 2, 2, 2, 3, 3, 3};
+		// ■ ■ ■
+		// ■ ■ ■
+		// ■ ■ ■
+		int[] x1 = { 1, 2, 3, 1, 2, 3, 1, 2, 3 };
+		int[] y1 = { 1, 1, 1, 2, 2, 2, 3, 3, 3 };
 		x = x1;
 		y = y1;
+	}
+
+	public void TwoStep() {
+
+		//   ■
+		// ■ ■
+		int[] x1 = { 1, 2, 2 };
+		int[] y1 = { 1, 1, 2 };
+		x = x1;
+		y = y1;
+
+	}
+	
+	public void DownTwoStep() {
+
+		// ■
+		// ■ ■
+		int[] x1 = { 1, 1, 2 };
+		int[] y1 = { 1, 2, 1 };
+		x = x1;
+		y = y1;
+
+	}
+
+	public void ThreeStep() {
+
+		//     ■
+		//   ■ ■
+		// ■ ■ ■
+		int[] x1 = { 1, 2, 2, 3, 3, 3 };
+		int[] y1 = { 1, 1, 2, 1, 2, 3 };
+		x = x1;
+		y = y1;
+
+	}
+
+	public void DownThreeStep() {
+
+		// ■
+		// ■ ■
+		// ■ ■ ■
+		int[] x1 = { 1, 1, 1, 2, 2, 3 };
+		int[] y1 = { 1, 2, 3, 1, 2, 1 };
+		x = x1;
+		y = y1;
+
+	}
+	
+	public void UpLittleT() {
+
+		// 
+		//   ■
+		// ■ ■ ■
+		int[] x1 = { 1, 2, 2, 3 };
+		int[] y1 = { 1, 1, 2, 1 };
+		x = x1;
+		y = y1;
+
+	}
+	
+	public void DownLittleT() {
+
+		// 
+		// ■ ■ ■
+		//   ■ 
+		int[] x1 = { 1, 2, 2, 3 };
+		int[] y1 = { 2, 1, 2, 2 };
+		x = x1;
+		y = y1;
+
+	}
+	
+	public void UpBigT() {
+
+		//   ■
+		//   ■
+		// ■ ■ ■
+		int[] x1 = { 1, 2, 2, 2, 3 };
+		int[] y1 = { 1, 1, 2, 3, 1 };
+		x = x1;
+		y = y1;
+
+	}
+	
+	public void DownBigT() {
+
+		// ■ ■ ■
+		//   ■ 
+		//   ■
+		int[] x1 = { 1, 2, 2, 2, 3 };
+		int[] y1 = { 3, 1, 2, 3, 3 };
+		x = x1;
+		y = y1;
+
+	}
+	
+	public void TwoxTwoPlusOne() {
+
+		// 
+		// ■ ■
+		// ■ ■ ■
+		int[] x1 = { 1, 1, 2, 2, 3 };
+		int[] y1 = { 1, 2, 1, 2, 1 };
+		x = x1;
+		y = y1;
+
+	}
+	
+	public void OnePlusTwoxTwo() {
+
+		// 
+		//   ■ ■
+		// ■ ■ ■
+		int[] x1 = { 1, 2, 2, 3, 3 };
+		int[] y1 = { 1, 1, 2, 1, 2 };
+		x = x1;
+		y = y1;
+
+	}
+	
+	public void ThreexTwoPlusOne() {
+
+		//   ■
+		// ■ ■ ■
+		// ■ ■ ■
+		int[] x1 = { 1, 1, 2, 2, 2, 3, 3 };
+		int[] y1 = { 1, 2, 1, 2, 3, 1, 2 };
+		x = x1;
+		y = y1;
+
 	}
 
 	public static void main(String[] args) {
--- Games/src/shapeJump/ShapePanel.java
+++ Games/src/shapeJump/ShapePanel.java
@@ -21,7 +21,7 @@ public class ShapePanel extends JPanel implements ActionListener, KeyListener {
 	private boolean playing = false;
 	private boolean endGame = false;
 	private boolean paused = false;
-	
+
 	private int shapeWidth = 20;
 	private int squareSide = shapeWidth;
 	private int circleDiameter = shapeWidth;
@@ -34,11 +34,21 @@ public class ShapePanel extends JPanel implements ActionListener, KeyListener {
 	// private int pos = nextBlockX();
 
 	private ArrayList<Integer> shapeGroupX = new ArrayList<Integer>();
-	
+	private ArrayList<Shape> shapeBlocks = new ArrayList<Shape>();
+
+	private int blockY = ground - 2;
+	private int deltaY = 0;
+	private int blockVel = 0;
+	private int gravity = 1;
+	private boolean jumping = false;
+
 	private int timeSplit;
 	private int timeSeconds;
+	private double timePressed = 0.0;
+	private double jumpTime;
 	private boolean UpPressed;
 	private boolean DownPressed;
+	
 
 	public ShapePanel() {
 
@@ -46,6 +56,10 @@ public class ShapePanel extends JPanel implements ActionListener, KeyListener {
 		shapeGroupX.add(500);
 		shapeGroupX.add(670);
 		shapeGroupX.add(840);
+
+		shapeBlocks.add(new Shape());
+		shapeBlocks.add(new Shape());
+		shapeBlocks.add(new Shape());
 
 		setFocusable(true);
 		addKeyListener(this);
@@ -65,28 +79,46 @@ public class ShapePanel extends JPanel implements ActionListener, KeyListener {
 	public void moves() {
 
 		if (playing) {
-		if (shapeGroupX.contains(0)) {
-			shapeGroupX.add(shapeGroupX.get(2) + 170);
-			shapeGroupX.remove(0);
-		}
+			if (shapeGroupX.contains(0)) {
+				shapeGroupX.add(shapeGroupX.get(2) + 170);
+				shapeGroupX.remove(0);
+				shapeBlocks.add(new Shape());
+				shapeBlocks.remove(0);
+			}
 
-		for (int i = 0; i < shapeGroupX.size(); i++) {
-			shapeGroupX.set(i, shapeGroupX.get(i) - shapeSpeed);
-		}
-		
-		timeSplit++;
-		if (timeSplit == 60) {
-			timeSplit = 0;
-			timeSeconds++;
-		}
-		
+			for (int i = 0; i < shapeGroupX.size(); i++) {
+				shapeGroupX.set(i, shapeGroupX.get(i) - shapeSpeed);
+			}
+			
+			if (blockY - deltaY <= ground && jumping) {
+				
+//				if (deltaY - gravity <= -18) {
+//					System.out.println(deltaY - gravity);
+//				}
+				deltaY -= gravity;
+				
+				
+			}
+			
+			if (blockY >= ground - 2 && jumping && deltaY < 0) {
+				blockY = ground - 1;
+				blockVel = 0;
+				jumping = false;
+				timePressed = 0;
+				deltaY = 0;
+			}
+
+			timeSplit++;
+			if (timeSplit == 60) {
+				timeSplit = 0;
+				timeSeconds++;
+			}
+			
+			blockY -= deltaY;
+
 		}
 
 		repaint();
-
-	}
-
-	public void drawShape(int randomShape) {
 
 	}
 
@@ -95,14 +127,16 @@ public class ShapePanel extends JPanel implements ActionListener, KeyListener {
 		// int pos = nextBlockX();
 		for (int i = 0; i < x.length; i++) {
 			g.setColor(Color.WHITE);
-			
+
 			int x1 = pos + (x[i] - 1) * blockSize;
 			int y1 = ground - y[i] * blockSize;
-			
-			g.fillRect(x1 + borderThickness, y1 + borderThickness, shapeWidth, shapeWidth);
-			
+
+			g.fillRect(x1 + borderThickness, y1 + borderThickness, shapeWidth,
+					shapeWidth);
+
 			g.setColor(Color.BLACK);
-			g.drawRect(x1 + borderThickness / 2, y1 + borderThickness / 2, shapeWidth, shapeWidth);
+			g.drawRect(x1 + borderThickness / 2, y1 + borderThickness / 2,
+					shapeWidth, shapeWidth);
 
 		}
 
@@ -113,15 +147,15 @@ public class ShapePanel extends JPanel implements ActionListener, KeyListener {
 		super.paintComponent(g);
 		g.setColor(Color.WHITE);
 		g.fillRect(0, ground, getWidth(), 10);
-		
+
 		if (startGame) {
-			
+
 			g.setFont(new Font("Joystix", Font.BOLD, 80));
-			CenteredText title1 = new CenteredText("SHAPE", 500, 500, g,
-					true, 130);
+			CenteredText title1 = new CenteredText("SHAPE", 500, 500, g, true,
+					130);
 			CenteredText title2 = new CenteredText("JUMPER", 500, 500, g, true,
 					200);
-			
+
 			g.setFont(new Font("Joystix", Font.BOLD, 20));
 
 			CenteredText start1 = new CenteredText("Press Enter to", 500, 500,
@@ -129,37 +163,31 @@ public class ShapePanel extends JPanel implements ActionListener, KeyListener {
 			// g.drawString("Press Enter to", 120, 300);
 			CenteredText start2 = new CenteredText("Start", 500, 500, g, true,
 					330);
-			
-			
+
 		} else if (playing) {
+			int i = 0;
+			for (Integer n : shapeGroupX) {
+				drawShape(shapeBlocks.get(i).x, shapeBlocks.get(i).y, n, g);
+				i++;
+			}
 
-		int[] x = { 1, 2, 1, 3, 2, 3 };
-		int[] y = { 1, 2, 3, 2, 3, 1 };
-
-		for (Integer n : shapeGroupX) {
-			drawShape(x, y, n, g);
-
-		}
-		
-		g.setColor(Color.WHITE);
-		g.setFont(new Font("Joystix", Font.BOLD, 20));
-		g.drawString(String.valueOf(timeSeconds), 5, 20);
-		
-		if (paused ) {
-			g.setFont(new Font("Joystix", Font.BOLD, 60));
-			CenteredText pause = new CenteredText("Paused", 500, 500,
-					g, true, 200);
-		}
-		
-		
-		
-		} else if (endGame) {
-			
-			
 			g.setColor(Color.WHITE);
 			g.setFont(new Font("Joystix", Font.BOLD, 20));
 			g.drawString(String.valueOf(timeSeconds), 5, 20);
-			
+
+			g.fillRect(20, blockY - 21, 20, 20);
+			if (paused) {
+				g.setFont(new Font("Joystix", Font.BOLD, 60));
+				CenteredText pause = new CenteredText("Paused", 500, 500, g,
+						true, 200);
+			}
+
+		} else if (endGame) {
+
+			g.setColor(Color.WHITE);
+			g.setFont(new Font("Joystix", Font.BOLD, 20));
+			g.drawString(String.valueOf(timeSeconds), 5, 20);
+
 			g.setFont(new Font("Joystix", Font.BOLD, 60));
 			g.setColor(Color.WHITE);
 			CenteredText lose = new CenteredText("You Lose!", 500, 500, g,
@@ -170,12 +198,7 @@ public class ShapePanel extends JPanel implements ActionListener, KeyListener {
 
 			CenteredText restart = new CenteredText("Enter to Restart", 500,
 					500, g, true, 320);
-			
-			
-			
-			
-			
-			
+
 		}
 
 	}
@@ -191,38 +214,62 @@ public class ShapePanel extends JPanel implements ActionListener, KeyListener {
 	@Override
 	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
-		if (e.getKeyCode() == KeyEvent.VK_UP) {
-			UpPressed = true;
 
+		
+		
+		if (e.getKeyChar() == 'w') {
+			
+			
+				jumping = true;
+				timePressed = getTimeHund();
+				deltaY = 20;
+			
 		} else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
 			DownPressed = true;
 
 		}
+		
+//		System.out.println("Hi");
+//		if (e.getKeyCode() == KeyEvent.VK_UP) {
+//			System.out.println("Pressed");
+//			if (UpPressed) {
+//				UpPressed = false;
+//				timePressed = getTimeHund();
+//				blockVel = 20;
+//			} else {
+//				UpPressed = true;
+//			}
+//		} else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+//			DownPressed = true;
+//
+//		}
+		
+		
 	}
 
 	@Override
 	public void keyPressed(KeyEvent e) {
 		// TODO Auto-generated method stub
-
+		
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
 		// TODO Auto-generated method stub
-		
+
 		if (e.getKeyCode() == KeyEvent.VK_UP) {
-			//UpPressed = false;
+			// UpPressed = false;
 
 		} else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
-			//DownPressed = false;
+			// DownPressed = false;
 
 		} else if (e.getKeyCode() == KeyEvent.VK_ENTER) {
 
-//			if (endGame) {
-//				
-//				reset();
-//
-//			}
+			// if (endGame) {
+			//
+			// reset();
+			//
+			// }
 			reset();
 			startGame = false;
 			endGame = false;
@@ -231,16 +278,26 @@ public class ShapePanel extends JPanel implements ActionListener, KeyListener {
 		}
 
 	}
-	
+
+	public double getTimeHund() {
+		System.out.println(((timeSeconds * 60) + timeSplit) * 5 / (3 * 100));
+		return ((timeSeconds * 60) + timeSplit) * 5 / 3;
+	}
+
 	public void reset() {
-		
-		
+
 		shapeGroupX.clear();
 		shapeGroupX.add(500);
 		shapeGroupX.add(670);
 		shapeGroupX.add(840);
+
+		shapeBlocks.clear();
+		shapeBlocks.add(new Shape());
+		shapeBlocks.add(new Shape());
+		shapeBlocks.add(new Shape());
+
 		timeSplit = 0;
 		timeSeconds = 0;
-		
+
 	}
 }
