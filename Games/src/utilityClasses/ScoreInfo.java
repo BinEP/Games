49b802941fb5a6cdd6480adddddc7abb5289e7c5
BinEP <bradystoffel@gmail.com>
2014-12-01 14:21:10 -0600

    Added score files

---
+++ Games/src/utilityClasses/ScoreInfo.java
@@ -0,0 +1,24 @@
+package utilityClasses;
+
+public class ScoreInfo {
+
+	
+	public ScoreInfo () {
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	public static void main(String[] args) {
+
+	}
+}
