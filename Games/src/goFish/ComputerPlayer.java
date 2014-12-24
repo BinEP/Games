e01db259c9371822476ffed17cbbe34602e1cd0c
BinEP <bradystoffel@gmail.com>
2014-12-24 15:35:33 -0600

    Stufff

---
+++ Games/src/goFish/ComputerPlayer.java
@@ -0,0 +1,53 @@
+package goFish;
+
+import java.util.ArrayList;
+
+public class ComputerPlayer {
+
+	ArrayList<Card> hand = new ArrayList<Card>();
+	public ComputerPlayer (ArrayList < Card > compHand) {
+		
+		hand = compHand;
+		
+		
+		
+	}
+	
+	
+	public ArrayList < Card > getGuess() {
+		
+		
+		
+		
+	}
+	
+	
+	public ArrayList < Card > getPairs() {
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	public int getNumOfPairs() {
+		
+		
+		for (Card c : hand) {
+			
+			
+			
+			
+			
+		}
+		
+		
+	}
+	
+	
+	public static void main(String[] args) {
+
+	}
+}
