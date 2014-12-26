a9f746f110ee2efb0e5e031052df9c2b9dfdfa99
BinEP <bradystoffel@gmail.com>
2014-12-26 13:01:22 -0600

    Stuff

--- Games/src/goFish/Card.java
+++ Games/src/goFish/Card.java
@@ -20,37 +20,47 @@ public class Card {
 		shown = getCardFace() + getSuitIcon();
 		col = Color.WHITE;
 		selected = false;
-		//hearts
-		//diamonds
-		//clubs
-		//spades
-		
+		// hearts
+		// diamonds
+		// clubs
+		// spades
+
 	}
-	
+
+	public Card(int s, int c) {
+
+		suit = s;
+		card = c;
+		shown = getCardFace() + getSuitIcon();
+		col = Color.WHITE;
+		selected = false;
+
+	}
+
 	public String getShown() {
-		
+
 		return shown;
-		
+
 	}
-	
+
 	public int getSuit() {
 		return suit;
 	}
-	
+
 	public Color getColor() {
 		return col;
 	}
-	
+
 	public Rectangle getRectangle() {
 		return picCard;
 	}
-	
+
 	public void setRectangle(int x, int y, int w, int h) {
 		picCard = new Rectangle(x, y, w, h);
 	}
-	
+
 	public void setColor(Color color) {
-		
+
 		col = color;
 	}
 
@@ -70,7 +80,7 @@ public class Card {
 			suitChar = "\u2660";
 			break;
 		}
-		
+
 		return suitChar;
 
 	}
@@ -82,7 +92,7 @@ public class Card {
 	}
 
 	public String getCardFace() {
-		
+
 		String cardFace = "A";
 		if (card < 11 && card != 1) {
 
@@ -111,55 +121,101 @@ public class Card {
 		}
 
 	}
-	
+
 	public static Comparator<Card> CardSuitComparator = new Comparator<Card>() {
 
 		public int compare(Card c1, Card c2) {
-		   int cardSuit1 = c1.getSuit();
-		   int cardSuit2 = c2.getSuit();
+			int cardSuit1 = c1.getSuit();
+			int cardSuit2 = c2.getSuit();
 
-		   //ascending order
-		   return cardSuit2 - cardSuit1;
+			// ascending order
+			return cardSuit2 - cardSuit1;
 
-		   //descending order
-		   //return StudentName2.compareTo(StudentName1);
-	    }};
-	    
-	    public static Comparator<Card> CardNumComparator = new Comparator<Card>() {
+			// descending order
+			// return StudentName2.compareTo(StudentName1);
+		}
+	};
 
-	    	public int compare(Card c1, Card c2) {
+	public static Comparator<Card> CardNumComparator = new Comparator<Card>() {
 
-	    	   int cardNum1 = c1.getCard();
-	    	   int cardNum2 = c2.getCard();
+		public int compare(Card c1, Card c2) {
 
-	    	   /*For ascending order*/
-	    	   return cardNum2-cardNum1;
+			int cardNum1 = c1.getCard();
+			int cardNum2 = c2.getCard();
 
-	    	   /*For descending order*/
-	    	   //rollno2-rollno1;
-	       }};
-	       
-	       @Override
-	       public String toString() {
-	           return card + getSuitIcon();
-	       }
-	       
-	       @Override
-	       public boolean equals(Object o) {
-	    	   
-	    	   if (o instanceof Card) {
-	    		   
-	    		   return getCard() == ((Card) o).getCard() && getSuit() == ((Card) o).getSuit();
-	    		   
-	    	   }
-	    	   return false;
-	    	   
-	       }
+			/* For ascending order */
+			return cardNum2 - cardNum1;
 
-	public static void main(String[] args) {
-		System.out.println('\u2665');
-		System.out.println("\u2663");
-		System.out.println("\u2666");
-		System.out.println("\u2660");
+			/* For descending order */
+			// rollno2-rollno1;
+		}
+	};
+
+	@Override
+	public String toString() {
+		return getCardFace() + getSuitIcon();
 	}
+
+	@Override
+	public boolean equals(Object o) {
+
+		if (o instanceof Card) {
+
+			return getCard() == ((Card) o).getCard()
+					&& getSuit() == ((Card) o).getSuit();
+
+		} else {
+			return false;
+		}
+	}
+
+	public boolean equalRank(Card card) {
+
+		return this.getCard() == card.getCard();
+
+	}
+
+	public boolean equalSuit(Card card) {
+
+		return this.getSuit() == card.getSuit();
+	}
+
+	// @Override
+	// public boolean equals(Object o) {
+	//
+	// StackTraceElement[] stackTraceElements = Thread.currentThread()
+	// .getStackTrace();
+	// if (o instanceof Card) {
+	// if (stackTraceElements[3].getMethodName().equals("contains")) {
+	// // for (int i = 0; i < stackTraceElements.length; i++) {
+	// // System.out.println(stackTraceElements[i].getMethodName());
+	// // }
+	// //
+	// System.out.println(stackTraceElements[stackTraceElements.length-1].getMethodName().toString());
+	//
+	// return getCard() == ((Card) o).getCard()
+	// && getSuit() == ((Card) o).getSuit();
+	// } else {
+	// return getCard() == ((Card) o).getCard();
+	// }
+	// } else {
+	// return false;
+	// }
+	// }
+
+	// @Override
+	// public boolean equals(Object o) {
+	//
+	// if (o instanceof Card) {
+	//
+	// return getCard() == ((Card) o).getCard();
+	//
+	// }
+	// return false;
+	//
+	// }
+
+	// public static void main(String[] args) {
+	// System.out.println(new Card());
+	// }
 }
--- Games/src/goFish/ComputerPlayer.java
+++ Games/src/goFish/ComputerPlayer.java
@@ -1,53 +1,99 @@
 package goFish;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class ComputerPlayer {
 
 	ArrayList<Card> hand = new ArrayList<Card>();
-	public ComputerPlayer (ArrayList < Card > compHand) {
-		
+	private ArrayList<Card> deck = new ArrayList<Card>();
+	private ArrayList<ArrayList<Card>> hands = new ArrayList<ArrayList<Card>>();
+
+	public ComputerPlayer(ArrayList<Card> compHand) {
+
 		hand = compHand;
-		
-		
-		
+
 	}
-	
-	
-	public ArrayList < Card > getGuess() {
-		
-		
-		
-		
+
+	public ComputerPlayer() {
+
 	}
-	
-	
-	public ArrayList < Card > getPairs() {
-		
-		
-		
-		
-		
-		
-		
-	}
-	
+
+	// public ArrayList<Card> getGuess() {
+	//
+	// }
+	//
+//	 public ArrayList<Card> getPairs(int pairNum) {
+//	
+//		 
+//		 
+//	 }
+
 	public int getNumOfPairs() {
-		
-		
-		for (Card c : hand) {
-			
-			
-			
-			
-			
+
+		ArrayList<Integer> matchNums = new ArrayList<Integer>();
+		for (Card c1 : hand) {
+			for (Card c2 : hand) {
+				if (c2.getCard() == c1.getCard() && !c2.equals(c1) && !matchNums.contains(c2.getCard()))
+					matchNums.add(c2.getCard());
+			}
 		}
-		
-		
+		return matchNums.size();
 	}
-	
-	
+
+	public void newHands() {
+		for (int n = 0; n < 2; n++) {
+			ArrayList<Card> hand1 = new ArrayList<Card>();
+			for (int i = 0; i < 7; i++) {
+				hand1.add(deck.get(0));
+				deck.remove(0);
+			}
+			Collections.sort(hand1, Card.CardSuitComparator);
+			Collections.sort(hand1, Card.CardNumComparator);
+
+			hands.add(hand1);
+			hand = hand1;
+		}
+		// System.out.println(hands);
+	}
+
+	public void newDeck() {
+		Card card = new Card();
+		for (int i = 0; i < 52; i++) {
+			while (deck.contains(card)) {
+				card = new Card();
+			}
+			deck.add(card);
+		}
+	}
+
+	public void shuffleDeck() {
+
+		Collections.shuffle(deck);
+	}
+
+	public void newGame() {
+
+		deck.clear();
+		hands.clear();
+		newDeck();
+		newHands();
+	}
+
+	public void testing() {
+
+		newGame();
+		System.out.println(hand);
+		System.out.println(getNumOfPairs());
+
+	}
+
 	public static void main(String[] args) {
+
+		ComputerPlayer cp = new ComputerPlayer();
+		for (int i = 0; i < 10; i++) {
+			cp.testing();
+		}
 
 	}
 }
