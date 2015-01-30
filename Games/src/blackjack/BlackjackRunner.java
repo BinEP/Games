e972306bfe0fe29377eea3e8ce033b986b353932
BinEP <bradystoffel@gmail.com>
2015-01-30 13:59:32 -0600

    Black Jack Panel name fix

--- Games/src/blackjack/BlackjackRunner.java
+++ Games/src/blackjack/BlackjackRunner.java
@@ -9,7 +9,7 @@ public class BlackjackRunner {
 	public static void main(String[] args) {
 
 		
-		JFrame frame = new JFrame("GO FISH");
+		JFrame frame = new JFrame("Black Jack");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setLayout(new BorderLayout());
 //		frame.setAlwaysOnTop(true);
