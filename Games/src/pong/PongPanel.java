cc1237f8c47d1295c7e3a6ef4d85f4a4bf841486
BinEP <bradystoffel@gmail.com>
2015-02-23 14:09:22 -0600

    Merge branch 'CodeCleanUp' into FrameworkConversion
    First stage of Cleanup with easier to convert games
    - Pong
    - Tunnel Runner
    - Snake
    - Hole In The Wall

--- Games/src/pong/PongPanel.java
+++ Games/src/pong/PongPanel.java
@@ -1,36 +1,22 @@
 package pong;
-import gameActions.Control;
 import gameActions.PlayerInterface;
 
-import java.awt.Color;
-import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.Rectangle;
 import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
+import java.awt.geom.Ellipse2D;
 import java.awt.Font;
-
-import javax.lang.model.element.Element;
-import javax.swing.JPanel;
-import javax.swing.Timer;
 
 import utilityClasses.CenteredText;
 
 public class PongPanel extends PlayerInterface {
 
-	//public int slopeX = (int) (Math.random() * 2) + 1;
-//	public int slopeX = rand4or8();
-//	public int slopeY = (int) (Math.random() * 2) + 1;
-	// ////////////////////////////////
 	public int startBallX = 400;
-	public int ballX = startBallX;
-	public int ballY = 250;
+	public Rectangle ball = new Rectangle(startBallX, 250, 20, 20);
+	
 	public int ballXSpeed = rand4or8();
 	public int ballYSpeed = rand4or8();
-	//public int ballXSpeed = (slopeX) * (4);
-		//public int ballYSpeed = (slopeY) * (4);
-	//public int deltaX = -Math.abs(((((int) (Math.random() * 2 )) * 2) - 1) * ballXSpeed);
+	
 	public int deltaX = randDeltaX();
 	public int deltaY = ((((int) (Math.random() * 2 )) * 2) - 1) * ballYSpeed;
 	public int diameter = 20;
@@ -38,159 +24,100 @@ public class PongPanel extends PlayerInterface {
 	public boolean computerPlayer = false;
 	
 	public int computerXMove = 200;
-	//public boolean nextBall = true;
-	//public int countDown = 3;
-	//public int countDownTiming = 20;
-
+	
 	public int paddleHeight = 100;
 	public int paddleWidth = 15;
 	public int paddleDistanceFromSide = 30;
 	public int paddleVerticalLocation = 200;
-	public int dashedY = 5;
-
+	public int paddleSpeed = 5;
+		
 	public int p1DistanceFromSide = paddleDistanceFromSide;
-	public int p2DistanceFromSide = width - paddleDistanceFromSide - 10;
-
-	// ///////////////////////////////////////
-
-	public int player1X = p1DistanceFromSide;
-	public int player1Y = paddleVerticalLocation;
-	public int player1Height = paddleHeight;
-	public int player1Width = paddleWidth;
-	public int player1MiddleY = player1Y + player1Height/2;
-
+	public int p2DistanceFromSide = width - 10 - paddleDistanceFromSide;
+	
+	public Rectangle paddleLeft = new Rectangle(p1DistanceFromSide, paddleVerticalLocation, paddleWidth, paddleHeight);
+	public Rectangle paddleRight = new Rectangle(p2DistanceFromSide, paddleVerticalLocation, paddleWidth, paddleHeight);
+	
 	public int player1Score = 0;
-
-	// ///////////////////////////////////////
-
-	public int player2X = p2DistanceFromSide;
-	public int player2Y = paddleVerticalLocation;
-	public int player2Height = paddleHeight;
-	public int player2Width = paddleWidth;
-
 	public int player2Score = 0;
 
-	// ///////////////////////////////////////
-
-	public int paddleSpeed = 5;
-
 	public PongPanel() {
-
 		super();
-		setSpeed(60);
-
 	}
+
 	@Override
 	public void moves() {
-
 		
-			//System.out.println(deltaX);
-			//System.out.println(deltaY);
-			//System.out.println(slopeX);
-			//System.out.println(slopeY);
-			if (leftPressed && player1Y - paddleSpeed > 50)
-				player1Y -= paddleSpeed;
-
-			if (rightPressed
-					&& player1Y + player1Height + paddleSpeed < getHeight() - 50)
-				player1Y += paddleSpeed;
-
-			if (upPressed && player2Y - paddleSpeed > 50)
-				player2Y -= paddleSpeed;
-
-			if (downPressed
-					&& player2Y + player2Height + paddleSpeed < getHeight() - 50)
-				player2Y += paddleSpeed;
-
-			int nextBallLeft = ballX + deltaX;
-			int nextBallRight = ballX + deltaX + diameter;
-			int nextBallTop = ballY + deltaY;
-			int nextBallBottom = ballY + deltaY + diameter;
-			int nextBallMid = ballY + deltaY + diameter/2;
-
-			int player1Right = player1X + player1Width;
-			int player1Top = player1Y;
-			int player1Bottom = player1Y + player1Height;
+		ball.x += deltaX;
+		ball.y += deltaY;
+		
+		if (upPressed && !paddleRight.contains(paddleRight.x + 5, 50)) {
+			paddleRight.translate(0, -paddleSpeed);
+		}
+		
+		if (downPressed && !paddleRight.contains(paddleRight.x + 5, height - 50)) {
+			paddleRight.translate(0, paddleSpeed);
+		}
+		
+		if (leftPressed && !paddleLeft.contains(paddleLeft.x + 5, 50)) {
+			paddleLeft.translate(0, -paddleSpeed);
+		}
+		
+		if (rightPressed && !paddleLeft.contains(paddleLeft.x + 5, height - 50)) {
+			paddleLeft.translate(0, paddleSpeed);
+		}
+		
+		if (computerPlayer) {
 			
-			int nextPlayer1Paddle = player1Y;
-			int nextPlayer1PaddleMid = player1Y + paddleSpeed;
-			
-			if (computerPlayer && nextPlayer1Paddle > nextBallMid && nextBallLeft < computerXMove) {
-				
+			if (paddleLeft.getMaxY() > ball.getCenterY()) {
+				left();
+			} else {
 				leftReleased();
 			}
-			if (computerPlayer && nextPlayer1PaddleMid < nextBallMid && nextBallLeft < computerXMove) {
+			if (paddleLeft.getMinY() < ball.getCenterY()) {
+				right();
+			} else {
 				rightReleased();
 			}
-			
-			int player2Left = player2X;
-			int player2Top = player2Y;
-			int player2Bottom = player2Y + player2Height;
-
-			if (nextBallTop < 0 || nextBallBottom > getHeight())
-				deltaY *= -1;
-
-			if (nextBallLeft < player1Right) {
-
-				if (!(nextBallTop > player1Bottom || nextBallBottom < player1Top)) {
-
-					deltaX *= -1;
-
-				} else if (nextBallLeft < 0) {
-					// System.out.println("Score Player 2!");
-					player2Score++;
-					ballXSpeed = rand4or8();
-					ballYSpeed = rand4or8();
-					//deltaX = ((((int) (Math.random() * 2 )) * 2) - 1) * ballXSpeed;
-					deltaX = randDeltaX();
-					deltaY = ((((int) (Math.random() * 2 )) * 2) - 1) * ballYSpeed;
-					ballX = startBallX;
-					ballY = 250;
-					//nextBall = false;
-				}
-			}
-
-			if (nextBallRight > player2Left) {
-
-				if (!(nextBallTop > player2Bottom || nextBallBottom < player2Top)) {
-
-					deltaX *= -1;
-
-				} else if (nextBallRight > getWidth()) {
-
-					// System.out.println("Score Player 1!");
-					player1Score++;
-					ballXSpeed = rand4or8();
-					ballYSpeed = rand4or8();
-					//deltaX = ((((int) (Math.random() * 2 )) * 2) - 1) * ballXSpeed;
-					deltaX = randDeltaX();
-					deltaY = ((((int) (Math.random() * 2 )) * 2) - 1) * ballYSpeed;
-					ballX = startBallX;
-					ballY = 250;
-					//nextBall = false;
-
-				}
-			}
-
-			ballX += deltaX;
-			ballY += deltaY;
+		}
 		
-
-
-		// ifStarted();
-
+		if (paddleLeft.intersects(ball) || paddleRight.intersects(ball)) {
+			deltaX *= -1;
+		}
+		
+		if (!outerbox.contains(ball)) {
+			if (outerbox.contains(ball.x, 250)) {
+				deltaY *= -1;
+			} else {
+				scored();
+			}	
+		}
 	}
+	
+	public void scored() {
+		
+		if (ball.x < 250) {
+			player2Score++;
+		} else {
+			player1Score++;
+		}
+		
+		ballXSpeed = rand4or8();
+		ballYSpeed = rand4or8();
+		deltaX = randDeltaX();
+		deltaY = ((((int) (Math.random() * 2 )) * 2) - 1) * ballYSpeed;
+		ball.x = startBallX;
+		ball.y = 250;
+	}
+	
 	@Override
 	public void drawPlaying(Graphics2D g) {
 		
-		g.fillOval(ballX, ballY, diameter, diameter);
-		g.fillRect(player1X, player1Y, player1Width, player1Height);
-		g.fillRect(player2X, player2Y, player2Width, player2Height);
-
-		/*
-		 * for (dashedY = 5; dashedY < getHeight(); dashedY += 34) {
-		 * g.fillRect(248, dashedY, 5, 15); }
-		 */
+		Ellipse2D.Double e = new Ellipse2D.Double(ball.x, ball.y, diameter, diameter);
+		
+		g.fill(e);
+		g.fill(paddleLeft);
+		g.fill(paddleRight);
+	
 		g.fillRect(248, 0, 5, getHeight());
 		g.setFont(new Font(Font.DIALOG, Font.BOLD, 36));
 		g.drawString(String.valueOf(player1Score), 205 - 32 * (spacing()),
@@ -198,6 +125,7 @@ public class PongPanel extends PlayerInterface {
 		g.drawString(String.valueOf(player2Score), 269, 420);
 		
 	}
+	
 	@Override
 	public void drawEnd(Graphics2D g) {
 		
@@ -206,68 +134,34 @@ public class PongPanel extends PlayerInterface {
 		String playerWon = (player1Score > player2Score) ? "1" : "2";
 		
 		if (!computerPlayer) {
-		CenteredText.draw("Player " + playerWon, 500, 500, g, true, 120);
-		CenteredText.draw("You Win!", 500, 500, g, true, 210);
-		//g.drawString("Player " + playerWon, playWon.x, 120);
+			CenteredText.draw("Player " + playerWon, 120, g);
+			CenteredText.draw("You Win!", 210, g);
 		} else {
-			
 			if (player1Score > player2Score) {
-			CenteredText.draw("Computer", 500, 500, g, true, 80);
-			CenteredText.draw("Won!", 500, 500, g, true, 150);
-			CenteredText.draw("You Lose!", 500, 500, g, true, 280);
-			
+				CenteredText.draw("Computer", 80, g);
+				CenteredText.draw("Won!", 150, g);
+				CenteredText.draw("You Lose!", 280, g);
 			} else {
-				
-				CenteredText.draw("You Win!", 500, 500, g, true, 150);
+				CenteredText.draw("You Win!", 150, g);
 			}
 			computerPlayer = false;
 		}
 		
-		
-		//g.drawString("You Win!", win.x, 210);
-		
 		g.setFont(new Font("Joystix", Font.BOLD, 26));
-		CenteredText.draw("Enter to restart", 500, 500, g, true, 350);
-		//g.drawString("Enter to Restart", restart.x, 320);
-		
-	}
-	@Override
-	public void customPressed(KeyEvent e) {
-
-		if (e.getKeyCode() == KeyEvent.VK_1 && (startGame || endGame)) {
-			
-			computerPlayer = true;
-			computerXMove = 200;
-			
-			
-		} else if (e.getKeyCode() == KeyEvent.VK_2 && (startGame || endGame)) {
-			
-			computerPlayer = true;
-			computerXMove = 400;
-			
-			
-		}
+		CenteredText.draw("Enter to restart", 350, g);
 	}
 
 	public int spacing() {
 		int spacer = player1Score / 10;
 		int spaces = 0;
 		while (spacer > 0) {
-
 			spaces++;
 			spacer /= 10;
-
 		}
-
-		// if (spacer > 0) spacer = 1;
-
 		return spaces;
-
 	}
 	public int rand4or8() {
 		return ((int) (Math.random() * 2) + 1) * (4);
-		
-		
 	}
 	
 	public int randDeltaX() {
@@ -283,14 +177,12 @@ public class PongPanel extends PlayerInterface {
 	
 	@Override
 	public boolean checkIfDead() {
-		// TODO Auto-generated method stub
 		return player1Score >= 11 || player2Score >= 11
 				&& Math.abs(player1Score - player2Score) > 1;
 	}
 
 	@Override
 	public void reset() {
-		// TODO Auto-generated method stub
 		setup();
 	}
 
@@ -298,35 +190,33 @@ public class PongPanel extends PlayerInterface {
 
 	@Override
 	public void setup() {
-		// TODO Auto-generated method stub
 		
 		leftKey = KeyEvent.VK_W;
 		rightKey = KeyEvent.VK_S;
+		
 		player1Score = 0;
 		player2Score = 0;
-		ballX = startBallX;
-		ballY = 250;
-		player1X = p1DistanceFromSide;
-		player1Y = paddleVerticalLocation;
-		player2X = p2DistanceFromSide;
-		player2Y = paddleVerticalLocation;
-		//countDown = 3;
-		//countDownTiming = 20;
+		
+		ball = new Rectangle(startBallX, 250, 20, 20);
+		ball.setLocation(startBallX, 250);
+		
+		paddleLeft = new Rectangle(p1DistanceFromSide, paddleVerticalLocation, paddleWidth, paddleHeight);
+		paddleRight = new Rectangle(width - 10 - p1DistanceFromSide, paddleVerticalLocation, paddleWidth, paddleHeight);
+
+		paddleLeft.setLocation(p1DistanceFromSide, paddleVerticalLocation);
+		paddleRight.setLocation(p2DistanceFromSide, paddleVerticalLocation);
+		
 		ballXSpeed = rand4or8();
 		ballYSpeed = rand4or8();
-		//deltaX = ((((int) (Math.random() * 2 )) * 2) - 1) * ballXSpeed;
+
 		deltaX = randDeltaX();
 		deltaY = ((((int) (Math.random() * 2 )) * 2) - 1) * ballYSpeed;
 	}
 
 	@Override
-	public void draw(Graphics2D g) {
-		// TODO Auto-generated method stub
-		
-	}
+	public void draw(Graphics2D g) {}
 	@Override
 	public String getGameName() {
-		// TODO Auto-generated method stub
 		return "Pong";
 	}
 
