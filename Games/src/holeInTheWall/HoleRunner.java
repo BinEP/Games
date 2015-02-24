342c11c1cd02fc0a864b921c70bcde537d1f913f
BinEP <bradystoffel@gmail.com>
2015-02-24 12:57:35 -0600

    Fullscreen Reformat

--- Games/src/holeInTheWall/HoleRunner.java
+++ Games/src/holeInTheWall/HoleRunner.java
@@ -7,13 +7,14 @@ import java.awt.BorderLayout;
 import javax.swing.JFrame;
 
 import pong.PongPanel;
+import snake.SnakePanel;
 
 public class HoleRunner {
 
 	public static void main(String[] args) {
 
-		
-	new Runner(new HolePanel());
+	Runner r = new Runner(new HolePanel());
+	Runner.enterFullScreen(r);
 	
 	}
 }
