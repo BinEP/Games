c9289189fcba2dd7bb2d4cdb49981f7eeaeffce9
BinEP <bradystoffel@gmail.com>
2015-02-24 12:57:23 -0600

    Full Screen Reformat

--- Games/src/tunnelRunner/TunnelRunner.java
+++ Games/src/tunnelRunner/TunnelRunner.java
@@ -6,13 +6,11 @@ import java.awt.BorderLayout;
 
 import javax.swing.JFrame;
 
-import pong.PongPanel;
-
 public class TunnelRunner {
 
 	public static void main(String[] args) {
 
-		
-		new Runner(new TunnelPanel());
+		Runner r = new Runner(new TunnelPanel());
+		Runner.enterFullScreen(r);
 	}
 }
